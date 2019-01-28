e300bb987bc3855e2addcd96b8010b0759136fdb
Toby Sullivan <tobyjsullivan@gmail.com>
2019-01-28 13:28:03 +1100

    Make requests against live API

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import { number } from 'prop-types';
+
+const API_URL = 'https://6chzxaw355.execute-api.ap-southeast-2.amazonaws.com/alpha/execute';
 
 interface AppState {
   formula: string,
+}
+
+interface ApiResult {
+  error: string,
+  result?: {
+    type: 'number';
+    numberValue: number;
+  } | {
+    type: 'string',
+    stringValue: string;
+  },
+}
+
+function execute(formula: string): Promise<number | string> {
+  return axios.post(API_URL, {
+    formula,
+  }).then((resp) => {
+    const payload: ApiResult = resp.data;
+
+    if (payload.error) {
+      throw payload.error;
+    }
+
+    if (!payload.result) {
+      throw 'expected result.';
+    }
+
+    switch (payload.result.type) {
+    case 'number':
+      return payload.result.numberValue;
+    case 'string':
+      return payload.result.stringValue;
+    }
+  });
 }
 
 class App extends Component {
   state = {
     varName: 'var1',
     formula: 'SUM(4,6)',
+    result: 'error: not implemented',
   }
 
   handleVarNameChanged = (varName: string) => {
@@ -16,30 +55,31 @@ class App extends Component {
   }
 
   handleFormulaChanged = (formula: string) => {
-    this.setState({formula});
+    this.setState({formula, result: 'executing...'});
+
+    execute(formula).then((result) => {
+      this.setState({result});
+    }).catch((e) => {
+      this.setState({result: `error: ${e}`});
+    });
   }
 
   render() {
-    const {varName, formula} = this.state;
+    const {varName, formula, result} = this.state;
 
     return (
       <div className="App">
         <h1 className="App-title">Chalk</h1>
         <div className="App-console">
           <p>
-            <input
-              className="App-var_name_input"
-              type="text"
-              defaultValue={varName}
-              onChange={e => this.handleVarNameChanged(e.target.value)} />
-            :&nbsp;
+            {varName}:&nbsp;
             <input
               className="App-formula_input"
               type="text"
               defaultValue={formula}
               onChange={e => this.handleFormulaChanged(e.target.value)} />
           </p>
-          <p>> error: not implemented</p>
+          <p>&gt; {result}</p>
         </div>
       </div>
     );
